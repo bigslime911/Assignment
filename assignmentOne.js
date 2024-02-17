c55cb798ece1612772c33078e6c0460f9e699258
bigslime892001 <donkorfrancis1000@icloud.com>
2024-02-17 03:19:35 +0000

    Substraction Operator

--- assignmentOne.js
+++ assignmentOne.js
@@ -3,3 +3,9 @@ function additionOperator(num1 , num2){
     return console.log(num1+num2)
 } 
 additionOperator(45,5)
+
+//Substraction Operator
+function substractionOperator(num1 , num2){
+    return console.log(num1-num2)
+} 
+substractionOperator(45,5)
